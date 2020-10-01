01fce7e2aad100512e62effa68c3e657938b117a
Kyle Norton <kylebnorton@gmail.com>
2020-09-30 22:51:32 -0600

    adding github pages files

---
+++ build/precache-manifest.6047895adb40fed89332eea5d02859d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8e68a521450004640968c9dcb2a838",
+    "url": "/movie-rolodex/index.html"
+  },
+  {
+    "revision": "8de8293e096f1739f2be",
+    "url": "/movie-rolodex/static/css/main.b137cdb0.chunk.css"
+  },
+  {
+    "revision": "a98031799a6b16b3da24",
+    "url": "/movie-rolodex/static/js/2.740ca5ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-rolodex/static/js/2.740ca5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de8293e096f1739f2be",
+    "url": "/movie-rolodex/static/js/main.3c5efcc2.chunk.js"
+  },
+  {
+    "revision": "9d8aa3e46f896527dcc3",
+    "url": "/movie-rolodex/static/js/runtime-main.e7324896.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import qs from 'qs';
 import axios from 'axios';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
@@ -15,22 +16,43 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.post('http://localhost:3001/retrieve', { type: 'popular' })
+    const query = qs.stringify({
+      api_key: 'ffa9544f9617b768fcb7fde474d40490',
+      language: 'en-US',
+      page: 1,
+      include_adult: false
+    });
+    axios.get(`https://api.themoviedb.org/3/movie/popular?${query}`)
       .then(response => this.setState({ movies: response.data.results }))
   }
 
-  getType = query => {
-    axios.post('http://localhost:3001/retrieve', { type: query })
+  getType = type => {
+    const query = qs.stringify({
+      api_key: 'ffa9544f9617b768fcb7fde474d40490',
+      language: 'en-US',
+      page: 1,
+      include_adult: false
+    });
+
+    axios.get(`https://api.themoviedb.org/3/movie/${type}?${query}`)
       .then(response => this.setState({ movies: response.data.results }))
   }
 
   handleSearch = e => {
-    axios.post('http://localhost:3001/search-movies', { e })
+    const query = qs.stringify({
+      api_key: 'ffa9544f9617b768fcb7fde474d40490',
+      language: 'en-US',
+      page: 1,
+      include_adult: false,
+      query: e
+    });
+    axios.get(`https://api.themoviedb.org/3/search/movie?${query}`)
       .then(response => this.setState({ movies: response.data.results }))
   }
   render() {
     return (
       <div className="App">
+        <h1>Movie Rolodex</h1>
         <SearchBox handleSearch={this.handleSearch} placeholder="search movies" />
         <button onClick={() => this.getType('top_rated')}>Top Rated</button>
         <button onClick={() => this.getType('upcoming')}>Upcoming</button>
